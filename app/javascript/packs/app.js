b57227d5847ec68fed00ffee2a5fd3fbd7508efc
waspryo <wasp2123321@gmail.com>
2019-12-31 23:04:45 +0900

    Delete Task API

--- app/javascript/packs/app.js
+++ app/javascript/packs/app.js
@@ -109,8 +109,10 @@ document.addEventListener("DOMContentLoaded", () => {
         let taskIndex = this.tasks.findIndex(item => item.id == id);
 
         if (taskIndex > -1) {
-          this.$delete(this.tasks, taskIndex);
-          this.message = `Task ${id} deleted.`;
+          Api.deleteTask(id).then(function(res) {
+            app.$delete(app.tasks, taskIndex);
+            app.message = `Task ${id} deleted.`;
+          });
         }
       }
     },
